1d015b0f416c7fa0e9bf0326d664c88626c57870
Krasimir Zorbov <49094272+krasizorbov@users.noreply.github.com>
2020-02-18 18:51:15 +0200

    Add files via upload

---
+++ 09. Iterators And Comparators Lab/BookComparer/IteratorsAndComparators/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+
+namespace IteratorsAndComparators
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 09. Iterators And Comparators Lab/Library/Library/StartUp.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+
+namespace IteratorsAndComparators
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+            Book bookTwo = new Book("The Documents in the Case", 2002, "Dorothy Sayers", "Robert Eustace");
+            Book bookThree = new Book("The Documents in the Case", 1930);
+
+            Library libraryOne = new Library();
+            Library libraryTwo = new Library(bookOne, bookTwo, bookThree);
+
+        }
+    }
+    public class Book
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public IReadOnlyList<string> Authors { get; set; }
+        public Book(string title, int year, params string[] authors)
+        {
+            Title = title;
+            Year = year;
+            Authors = authors;
+        }
+    }
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            foreach (var book in books)
+            {
+                yield return book;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
